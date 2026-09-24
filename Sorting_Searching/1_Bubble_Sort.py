ed0587ccbae58af2232b6558b425d36c850d2bda
ranish396@gmail.com <ranishpriya396.com>
2026-09-24 13:24:48 +0530

    new_file

---
+++ Sorting_Searching/1_Bubble_Sort.py
@@ -0,0 +1,10 @@
+nums = [1,11,9,10,3,4,5]
+def bubble_sort(nums):
+    for i in range(0,len(nums)):
+        swapped = False
+        for j in range(len(nums)-i-1):
+            if nums[j]> nums[j+1]:
+                nums[j],nums[j+1] = nums[j+1], nums[j]
+                swapped = True 
+        
+
